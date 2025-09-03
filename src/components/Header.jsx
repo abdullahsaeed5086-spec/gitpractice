0a66372d50e734a58713f31215b60146ccdec44b
abdullah <you@example.com>
2025-09-03 09:16:52 +0500

    APPLy

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+<h1>ABDULLAH SAEED</h1>
+<input type='text'placeholder='USERNAME'/>
+<button type='button' style={{backgroundColor:"yellow", width:"89px"}}></button>
+
+
+    </div>
+  )
+}
+
+export default Header
