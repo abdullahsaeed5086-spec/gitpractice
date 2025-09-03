f4b6dca07bef039b3ec8d9c9795e094fe3921312
Abubakar <abubakraslam177@gmail.com>
2025-09-03 10:07:36 +0500

    abubakar-branch

---
+++ src/components/aos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const aos = () => {
+  return (
+    <>
+    <h1>Abubakar Aslam</h1>
+    <div data-aos="flip-left">flip-left</div>
+    <div data-aos="flip-right">"flip-right</div>
+    </>
+  )
+}
+
+export default aos
